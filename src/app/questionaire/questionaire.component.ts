02a9ba681191c08beeeb724a52cea1f6a6bb2a78
kellysun98 <kelly.sunyixin@gmail.com>
2020-09-24 21:39:30 -0400

    popup window v1

---
+++ src/app/questionaire/questionaire.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {MatTableDataSource} from '@angular/material/table';
+
+interface DialogData {
+  email: string;
+}
+
+@Component({
+  selector: 'app-questionaire',
+  templateUrl: './questionaire.component.html',
+  styleUrls: ['./questionaire.component.css']
+})
+export class QuestionaireComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<QuestionaireComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit() {
+  }
+
+}
