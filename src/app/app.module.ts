cb1303c98448b4b05c7d96fbb33516ec69fa4e28
yqy0311 <yqy0311@gmail.com>
2020-09-27 18:08:33 +0800

    1808

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule} from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { QuestionaireComponent } from './questionaire/questionaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionaireComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
     HttpClientModule,
-    MatDialogModule
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatCheckboxModule
   ],
+  exports:[MatFormFieldModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,QuestionaireComponent]
 })
 export class AppModule { }
 
