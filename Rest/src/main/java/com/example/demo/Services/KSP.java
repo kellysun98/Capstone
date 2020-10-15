2a1e048c8a1359391b3ffabca4cf1e7ab160da31
helenwangd <helendyh.wang@mail.utoronto.ca>
2020-10-14 23:02:36 -0400

    Route search now work with user questionnaire answer

--- Rest/src/main/java/com/example/demo/Services/KSP.java
+++ Rest/src/main/java/com/example/demo/Services/KSP.java
@@ -19,7 +19,6 @@ public class KSP {
 
     public static ArrayList<Path> ksp(Graph graph, MapNode src, MapNode dest, String costFunction, int K) {
 
-
         ArrayList<Path> A = new ArrayList<>();
 
         // Initialize a set to store potential kth shortest path
@@ -70,16 +69,7 @@ public class KSP {
             // Add the lowest cost path becomes the k-shortest path
             A.add(B.get(0));
             B.remove(0);
-//            System.out.println("A size = " + A.size());
-//            System.out.println("A: " + A);
-//            System.out.println("B size = " + B.size());
-//            System.out.println("B[0]: " + B.get(0));
         }
-        // Print cost of each path in A for sanity check
-//        for (int j = 0; j < A.size(); j++) {
-//            System.out.println("Cost of A[" + j + "]= " + A.get(j).getTotalCost());
-//        }
-//        A.get(0)
         return A;
     }
 
@@ -120,9 +110,53 @@ public class KSP {
 
         // Find shortest path from src to sink
         Planner planner = new Planner();
-        Path pure_distance_shortestpath = planner.plan(graph, src, dest, "distance");
         Path shortestpath = planner.plan(graph, src, dest, costFunction);
         A.add(shortestpath);
+        if (detour_time == -1.0){ // Case 1: user choose "No Time Limit"
+//            System.out.println("I am in the case of -1.0");
+            for (int k = 1; k < K; k++) {
+                // The spur node ranges from the first node to the next to last node in the previous k-shortest path.
+                Path previousPath = A.get(k - 1);
+                for (int i = 0; i < previousPath.size() - 2; i++) {
+                    // Spur node is retrieved from the previous k-shortest path, k − 1.
+                    MapNode spurNode = previousPath.get(i);
+                    // The sequence of nodes from the source to the spur node of the previous k-shortest path.
+                    Path rootPath = previousPath.subPath(0, i);
+                    // Clone spurNode for restore purpose
+                    //MapNode spurNode_copy = spurNode.clone();
+                    MapNode spurNode_copy = new MapNode(spurNode);
+
+
+                    // Remove edges
+                    for (int n = 0; n < previousPath.size(); n++) {
+                        if (previousPath.getNodes().get(n).equals(spurNode)) {
+                            spurNode_copy.removeEdges(previousPath.get(n + 1));
+                        }
+                    }
+
+                    // Calculate the spur path from spurNode to destinationNode
+                    Path spurPath = planner.plan(graph, spurNode_copy, dest, costFunction);
+
+
+                    // If a new spur path is found
+                    if (spurPath != null) {
+                        // Entire path is made up of root path and spur path
+                        Path totalPath = Path.concatenate(rootPath, spurPath);
+
+                        // Add potential k-shortest path to the heap
+                        if (!B.contains(totalPath))
+                            B.add(totalPath);
+                    }
+                }
+                if (B.isEmpty())
+                    break;
+                Collections.sort(B);
+                // Add the lowest cost path becomes the k-shortest path
+                A.add(B.get(0));
+                B.remove(0);
+            }
+        }else{
+        Path pure_distance_shortestpath = planner.plan(graph, src, dest, "distance");
         if (shortestpath.getTotalCost()>=(pure_distance_shortestpath.getTotalCost()+detour_distance))
             System.out.println("Can't find any lower risk routes within detour time limit, here are lower risk routes with possible minimal detour time:");
 
@@ -165,18 +199,9 @@ public class KSP {
             // Add the lowest cost path becomes the k-shortest path
             A.add(B.get(0));
             B.remove(0);
-//            System.out.println("A size = " + A.size());
-//            System.out.println("A: " + A);
-//            System.out.println("B size = " + B.size());
-//            System.out.println("B[0]: " + B.get(0));
         }
-        // Print cost of each path in A for sanity check
-//        for (int j = 0; j < A.size(); j++) {
-//            System.out.println("Cost of A[" + j + "]= " + A.get(j).getTotalCost());
-//        }
-//        A.get(0)
+        }
         return A;
-
 
     }
 
